b9539027bfdf2868400b6f9ba4364240c5505fa4
Flandre <971056445@qq.com>
2018-04-25 18:25:25 +0800

    add

--- js/base.js
+++ js/base.js
@@ -1,2 +1,7 @@
+const B64 = new Hashes.Base64
+const CRC32 = new Hashes.CRC32
 const MD5 = new Hashes.MD5
+const RMD160 = new Hashes.RMD160
 const SHA1 = new Hashes.SHA1
+const SHA256 = new Hashes.SHA256
+const SHA612 = new Hashes.SHA612
