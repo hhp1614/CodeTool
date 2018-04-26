66bcd9c578f503b829fd9cdced1f67f3ead6e3f4
Flandre <971056445@qq.com>
2018-04-26 18:33:02 +0800

    delete base.js

--- js/base.js
+++
@@ -1,7 +0,0 @@
-const B64 = new Hashes.Base64
-const CRC32 = new Hashes.CRC32
-const MD5 = new Hashes.MD5
-const RMD160 = new Hashes.RMD160
-const SHA1 = new Hashes.SHA1
-const SHA256 = new Hashes.SHA256
-const SHA512 = new Hashes.SHA512
